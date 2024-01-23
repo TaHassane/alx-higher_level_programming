1f199caa6532623e9c6966a6519739ace064892b
TaHassane <m.taha.hassane@gmail.com>
2024-01-22 17:19:02 -0800

    Tsk 0: fix requirement

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-
+    printed = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
+            printed += 1
         except:
-            break
-        else:
-            count += 1
-
+            continue
     print()
-    return (count)
+    return printed
