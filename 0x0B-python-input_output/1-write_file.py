2a8fc1b61d4a1cba78a2f1fdb0be5c3f8bc2fb09
TaHassane <m.taha.hassane@gmail.com>
2024-02-07 00:14:08 +0000

    Input Output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" Module that contains a function that reads from a file """
+""" Module that contains a function that returns the number of lines
+    of a text file
+"""
 
 
-def read_file(filename=""):
-    """ Function that reads from a file
+def number_of_lines(filename=""):
+    """ Function that reads from a file and prints its number of lines
 
     Args:
         filename: filename
@@ -12,7 +14,8 @@ def read_file(filename=""):
         Exception: when the file can be opened
 
     """
-
+    n_lines = 0
     with open(filename, 'r', encoding="utf-8") as f:
-        read_data = f.read()
-        print(read_data, end='')
+        for line in f:
+            n_lines += 1
+    return n_lines
