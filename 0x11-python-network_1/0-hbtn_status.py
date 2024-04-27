498ff1cd9acfad441b745ec60337b53536301d00
TaHassane <m.taha.hassane@gmail.com>
2024-04-26 17:37:38 -0700

    py net

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ fetch https://alx-intranet.hbtn.io/status """
+if __name__ == "__main__":
+    from urllib import request
 
-if __name__ == '__main__':
-    import urllib.request
-    with urllib.request.urlopen('https://alx-ntranet.hbtn.io/status') as response:
-        body = response.read()
-    print('Body response:\n\t- type: {}'.format(type(body)))
-    print('\t- content: {}'.format(body))
-    print('\t- utf8 content: {}'.format(body.decode('utf-8')))
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode('utf-8')))
