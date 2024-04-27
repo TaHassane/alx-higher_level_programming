eb35cde68562fbfe1cb8c0f18ad6bf57101993ec
TaHassane <m.taha.hassane@gmail.com>
2024-04-26 17:39:21 -0700

    py net

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Fetch https://intranet.hbtn.io/status using the requests package"""
+""" Fetch https://alx-intranet.hbtn.io/status using the requests package"""
 
 if __name__ == '__main__':
     import requests
-    reqst = requests.get('https://intranet.hbtn.io/status')
+    reqst = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:\n\t- type: {}'.format(type(reqst.text)))
     print('\t- content: {}'.format(reqst.text))
