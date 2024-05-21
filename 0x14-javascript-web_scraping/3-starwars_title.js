cfd10d6227db8748b45de57980ae62b21cd7464e
TaHassane <m.taha.hassane@gmail.com>
2024-05-21 14:46:29 -0700

    Js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* Task 3 - script that prints the title of a Star Wars movie where the episode number matches a given integer. */
+const request = require('request');
+const url = 'http://swapi-api.hbtn.io/api/films/' + parseInt(process.argv[2]);
+request(url, function (error, response, body) {
+  if (error) throw error;
+  console.log(JSON.parse(body).title);
+});
---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*  script that gets the contents of a webpage and stores it in a file. */
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) throw error;
+  fs.writeFile(process.argv[3], body, 'utf8', function (err, data) {
+    if (err) throw err;
+  });
+});
